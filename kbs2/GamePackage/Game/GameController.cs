e34f4f0c0ab2ba68fdd80795ec35513196f35402
Jan Mouwes <jan.mouwes@gmail.com>
2018-11-16 16:43:15 +0100

    Added: GameStateEventArgs, GameController's methods

--- kbs2/GamePackage/Game/GameController.cs
+++ kbs2/GamePackage/Game/GameController.cs
@@ -1,11 +1,28 @@
+using kbs2.Desktop.GamePackage.EventArgs;
 using kbs2.GamePackage;
 
 namespace kbs2.Desktop.GamePackage.Game
 {
+    public delegate void GameStateObserver(object sender, GameStateEventArgs eventArgs);
+
     public class GameController
     {
         //private GameTimer Timer;
 
         public GameSpeed Speed { get; set; }
+
+        private GameState gameState;
+
+        public GameState GameState
+        {
+            get => gameState;
+            set
+            {
+                gameState = value;
+                GameStateChange?.Invoke(this, new GameStateEventArgs(gameState)); //Invoke event if has subscribers
+            }
+        }
+
+        public event GameStateObserver GameStateChange;
     }
 }
