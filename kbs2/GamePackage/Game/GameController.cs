e133c096b716d38493d945c08b8740b5f686fb4f
Jan Mouwes <jan.mouwes@gmail.com>
2018-11-16 16:35:34 +0100

    Added: GameSpeed, GameController

--- kbs2/GamePackage/Game/GameController.cs
+++ kbs2/GamePackage/Game/GameController.cs
@@ -1,7 +1,11 @@
+using kbs2.GamePackage;
+
 namespace kbs2.Desktop.GamePackage.Game
 {
     public class GameController
     {
         //private GameTimer Timer;
+
+        public GameSpeed Speed { get; set; }
     }
 }
