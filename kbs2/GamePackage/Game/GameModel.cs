bebbc92cc2a3415bbbdcc53968859e9c9c033c4f
chris <s1116195@student.windesheim.nl>
2018-11-19 12:05:37 +0100

    made GameModel

---
+++ kbs2/GamePackage/Game/GameModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kbs2.Desktop.World.World;
+
+namespace kbs2.GamePackage.Game
+{
+	class GameModel
+	{
+		public WorldController World { get; set; }
+		public List<Faction> Factions { get; set; }
+		public float Time { get; set; }
+		public GameState GameState { get; set; }
+		public Enum GameSpeed { get; set; }
+	}
+}
