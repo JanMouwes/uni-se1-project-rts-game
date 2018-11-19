a7b5fd8b5df3be9172a14957bd8f39007b4d4739
chris <s1116195@student.windesheim.nl>
2018-11-19 16:02:20 +0100

    added refrence

--- kbs2/GamePackage/Game/GameModel.cs
+++ kbs2/GamePackage/Game/GameModel.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using kbs2.Desktop.World.World;
+using kbs2.Faction.FactionMVC;
 
 namespace kbs2.GamePackage.Game
 {
 	class GameModel
 	{
 		public WorldController World { get; set; }
-		public List<Faction> Factions { get; set; }
+		public List<Faction_Controller> Factions { get; set; }
 		public float Time { get; set; }
 		public GameState GameState { get; set; }
 		public Enum GameSpeed { get; set; }
