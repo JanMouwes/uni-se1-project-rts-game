8c824c50217e824998e0b3a17211f5f941052cf3
Niels van Warmerdam <nielswarmerdam@hotmail.com>
2018-11-19 12:22:33 +0100

    Faction met relationship, units en buildings en alle interfaces aangemaakt

---
+++ kbs2/Faction/Enums/Faction_Relations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kbs2.Faction.Enums
+{
+    public enum Faction_Relations
+    {
+        friendly,
+        neutral,
+        hostile
+    }
+}
