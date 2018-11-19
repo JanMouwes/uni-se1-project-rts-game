04fe62ab3df052ab92f053369664cc5e735b0ce0
Niels van Warmerdam <nielswarmerdam@hotmail.com>
2018-11-19 15:41:28 +0100

    NUnit test FactionTests added

---
+++ Tests/FactionTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Prime.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestFixture]
+    public class FactionTests
+    {
+        [Test]
+        public void a()
+        {
+            
+        }
+    }
+}
