cf3c2c14b0623dfdafcef6d45f67a28becd2b85d
Jan Mouwes <jan.mouwes@gmail.com>
2018-11-20 14:51:10 +0100

    Added: Test-package

--- kbs2/World/Chunk/WorldChunkModel.cs
+++ kbs2/World/Chunk/WorldChunkModel.cs
@@ -7,9 +7,18 @@ using kbs2.World.Cell;
 
 namespace kbs2.World.Chunk
 {
-	public class WorldChunkModel
-	{
-		public WorldCellModel[,] grid;
-		public Coords coords;
-	}
+    public class WorldChunkModel
+    {
+        public const int ChunkSize = 20;
+
+        public WorldCellModel[,] grid;
+        public Coords coords;
+
+        public WorldChunkModel(Coords coords)
+        {
+            this.grid = new WorldCellModel[ChunkSize, ChunkSize];
+
+            this.coords = coords;
+        }
+    }
 }
--- kbs2/World/World/WorldModel.cs
+++ kbs2/World/World/WorldModel.cs
@@ -7,8 +7,5 @@ namespace kbs2.Desktop.World.World
 	public class WorldModel
 	{
 		public Dictionary<Coords, WorldChunkController> ChunkGrid { get; set; }
-		//old chunkGrid property. not able to extend bounderies because its an array. remove if not used.
-        // public WorldChunkController[,] ChunkGrid { get; set; }
-		public int ChunkSize { get; set; }
     }
 }
