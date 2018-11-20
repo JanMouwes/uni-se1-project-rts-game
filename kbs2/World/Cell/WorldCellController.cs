831e3b3d3f02ae30e99ffa566b3a315288eff512
chris <s1116195@student.windesheim.nl>
2018-11-20 11:11:38 +0100

    added comments to worldCellController

--- kbs2/World/Cell/WorldCellController.cs
+++ kbs2/World/Cell/WorldCellController.cs
@@ -11,18 +11,22 @@ namespace kbs2.World.Cell
 	{
 		public WorldCellModel worldCellModel { get; set; }
 
+        // Changes the TerrainType of the current cell
         public void ChangeTerrain(TerrainType type)
         {
         }
 
+        // Linkes the building on top of the cell to the model
         public void Construct(IConstructable constructable)
         {
         }
 
-        public void ChangeView(ViewMode mode)
+        // Switches the viewmode between enum ViewMode ( full, fog and none )
+        public void ChangeViewMode(ViewMode mode)
         {
         }
 
+        // This function is called when the building on top of the cell is destroyed
         public void OnDestruction()
         {
         }
