20694c2711323436a8f7112c63a1bc87bc71d8ff
marijnkoel <marijnkoel@live.nl>
2018-11-21 15:42:31 +0100

    refactor structs

---
+++ kbs2/WorldEntity/Structs/WeightDictionarys.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using kbs2.World;
+
+namespace kbs2.WorldEntity.Structs
+{
+    public struct WeightDictionarys
+    {
+        public WeightDictionarys(bool shit)// bool does nothing but is required
+        {
+            CellsWithWeight = new Dictionary<Coords, CellWeight>();
+            BorderCellsWithWeight = new Dictionary<Coords, CellWeight>();
+            ObstacleList = new List<Coords>();
+        }
+
+        public Dictionary<Coords, CellWeight> CellsWithWeight;
+        public Dictionary<Coords, CellWeight> BorderCellsWithWeight;
+        public List<Coords> ObstacleList;
+    }
+}
