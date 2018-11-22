88f6194a0ecd33ce2ccb07c47843b1d6dee6aa86
chris <s1116195@student.windesheim.nl>
2018-11-22 16:42:23 +0100

    added comments to worldFactory

--- kbs2/World/World/WorldFactory.cs
+++ kbs2/World/World/WorldFactory.cs
@@ -12,33 +12,47 @@ namespace kbs2.World.World
     {
         public static WorldController GetNewWorld()
         {
+            // Initialize World 
             WorldController world = new WorldController();
+
+            // Initialize WorldModel and ChunkGrid in World so you can add chunks to this grid
             world.worldModel = new WorldModel
             {
                 ChunkGrid = new Dictionary<Coords, WorldChunkController>()
             };
 
+            // For loop to add chunks from -2 to +2 in both x and y directions. this makes for a 5x5 chunkmap.
             for(int x = -2; x < 2; x++)
             {
                 for (int y = -2; y < 2; y++)
                 {
+                    // Sets the coords for the new chunk
                     Coords coords = new Coords { x = x, y = y };
+
+                    // Initializes a new chunk with the set coords and a basic terrain type
                     WorldChunkController chunkController = WorldChunkFactory.ChunkOfTerrainType(coords, TerrainType.Sand);
+
+                    // Adds the new chunk to the worldgrid
                     world.worldModel.ChunkGrid.Add(coords, chunkController);
                 }
             }
 
+            // Returns the worldController with a 5x5 chunkGrid initialized
             return world;
         }
 
         public static WorldController GetNewEmptyWorld()
         {
+            // Initialize World 
             WorldController world = new WorldController();
+
+            // Initialize WorldModel and ChunkGrid in World so you can add chunks to this grid
             world.worldModel = new WorldModel
             {
                 ChunkGrid = new Dictionary<Coords, WorldChunkController>()
             };
 
+            // Returns the initialized worldController without chunks loaded
             return world;
         }
     }
