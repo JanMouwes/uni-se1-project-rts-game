649a151e2815a8866204a541f4b28bc941dea4c7
Niels van Warmerdam <nielswarmerdam@hotmail.com>
2018-11-22 15:28:17 +0100

    interfaces geimplementeerd in unitmodel

--- kbs2/WorldEntity/Unit/MVC/Unit_Model.cs
+++ kbs2/WorldEntity/Unit/MVC/Unit_Model.cs
@@ -8,10 +8,12 @@ using kbs2.WorldEntity.Battle;
 using kbs2.WorldEntity.Health;
 using kbs2.WorldEntity.Location;
 using kbs2.WorldEntity.XP.XPMVC;
+using kbs2.Unit.Interfaces;
+using kbs2.Unit.Abstract;
 
 namespace kbs2.Unit.Model
 {
-    public class Unit_Model
+    public class Unit_Model : IHasPersonalSpace, IPurchasable, IClickable, IElemental
     {
 		public Faction_Model FactionModel;
 		public UnitDef UnitDef;
@@ -20,7 +22,16 @@ namespace kbs2.Unit.Model
 		public Battle_Model BattleModel;
 		public Location_Model LocationModel;
 
-		public Unit_Model() { }
+        public List<TerrainType> UnwalkableTerrain { get; set; }
+
+        public List<ElementType> Elementtypes { get; set; }
+
+        public CostValue CostValue { get; set; }
+
+        public Hitbox Clickbox { get; set; }
+        
+
+        public Unit_Model() { }
 
         public void InsertUnitDef(int id)
         {
@@ -32,6 +43,6 @@ namespace kbs2.Unit.Model
             Con.CloseConnection();
         }
 
-        public List<TerrainType> UnwalkableTerrain { get; set; }
+        
     }
 }
--- kbs2/WorldEntity/Unit/MVC/Unit_View.cs
+++ kbs2/WorldEntity/Unit/MVC/Unit_View.cs
@@ -1,13 +1,33 @@
-﻿using System;
+﻿using kbs2.Unit.Model;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace kbs2.WorldEntity.Unit.MVC
 {
 	public class Unit_View
 	{
+        public Unit_Controller UnitController { get; set; }
+        public Unit_Model UnitModel { get; set; }
+        public Texture2D Shape { get; set; }
+        public string ImageSrc { get; set; }
+        public float Height { get; set; }
+        public float Width { get; set; }
 
+        public Unit_View(string image, float height, float width)
+        {
+            ImageSrc = image;
+            Height = height;
+            Width = width;
+        }
+
+        public void Draw()
+        {
+            
+        }
 	}
 }
