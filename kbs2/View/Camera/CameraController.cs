563c28f056ef26caa1cbfd2e1a854ef197f1292f
chris <s1116195@student.windesheim.nl>
2018-11-23 14:36:25 +0100

    spit responsibilities some more

--- kbs2/View/Camera/CameraController.cs
+++ kbs2/View/Camera/CameraController.cs
@@ -1,6 +1,9 @@
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using MonoGame.Extended;
 using MonoGame.Extended.ViewportAdapters;
+using System;
 
 namespace kbs2.Desktop.View.Camera
 {
@@ -14,5 +17,42 @@ namespace kbs2.Desktop.View.Camera
         public CameraController(ViewportAdapter viewportAdapter) : base(viewportAdapter) { }
 
         public CameraModel cameraModel { get; set; }
+
+        /// <summary>
+        /// Checks keys for moving the camera and updates the camera.
+        /// </summary>
+        public void MoveCamera()
+        {
+            Vector2 moveVelocity = Vector2.Zero;
+
+            const float moveSpeed = 1.5f;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right)) moveVelocity += new Vector2(moveSpeed, 0);
+            if (Keyboard.GetState().IsKeyDown(Keys.Down)) moveVelocity += new Vector2(0, moveSpeed);
+            if (Keyboard.GetState().IsKeyDown(Keys.Left)) moveVelocity += new Vector2(-moveSpeed, 0);
+            if (Keyboard.GetState().IsKeyDown(Keys.Up)) moveVelocity += new Vector2(0, -moveSpeed);
+            if (Keyboard.GetState().IsKeyDown(Keys.G)) ZoomOut((float)0.1);
+            if (Keyboard.GetState().IsKeyDown(Keys.H)) ZoomIn((float)0.1);
+
+            UpdateZoom();
+
+            Move(moveVelocity);
+        }
+
+        /// <summary>
+        /// Checks for zoom updates and changes zoom accordingly.
+        /// </summary>
+        public void UpdateZoom()
+        {
+            int currentScrollWheelValue = Mouse.GetState().ScrollWheelValue;
+            int scrollChange = cameraModel.PreviousScrollWheelValue - currentScrollWheelValue;
+            double zoomChange = scrollChange / 36000.0;
+
+            ZoomOut((float)zoomChange);
+
+            cameraModel.PreviousScrollWheelValue = currentScrollWheelValue;
+
+            if (Math.Abs(zoomChange) < 0.000001) return;
+        }
     }
 }
--- kbs2/View/MapView/MapView.cs
+++ kbs2/View/MapView/MapView.cs
@@ -82,46 +82,9 @@ namespace kbs2.Desktop.View.MapView
                 Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            MoveCamera();
+            Camera.MoveCamera();
 
             base.Update(gameTime);
-        }
-
-        /// <summary>
-        /// Checks keys for moving the camera and updates the camera.
-        /// </summary>
-        private void MoveCamera()
-        {
-            Vector2 moveVelocity = Vector2.Zero;
-
-            const float moveSpeed = 1.5f;
-
-            if (Keyboard.GetState().IsKeyDown(Keys.Right)) moveVelocity += new Vector2(moveSpeed, 0);
-            if (Keyboard.GetState().IsKeyDown(Keys.Down)) moveVelocity += new Vector2(0, moveSpeed);
-            if (Keyboard.GetState().IsKeyDown(Keys.Left)) moveVelocity += new Vector2(-moveSpeed, 0);
-            if (Keyboard.GetState().IsKeyDown(Keys.Up)) moveVelocity += new Vector2(0, -moveSpeed);
-            if (Keyboard.GetState().IsKeyDown(Keys.G)) Camera.ZoomOut((float)0.1);
-            if (Keyboard.GetState().IsKeyDown(Keys.H)) Camera.ZoomIn((float)0.1);
-
-            updateZoom();
-
-            Camera.Move(moveVelocity);
-        }
-
-        /// <summary>
-        /// Checks for zoom updates and changes zoom accordingly.
-        /// </summary>
-        private void updateZoom()
-        {
-            int currentScrollWheelValue = Mouse.GetState().ScrollWheelValue;
-            int scrollChange = Camera.cameraModel.PreviousScrollWheelValue - currentScrollWheelValue;
-            double zoomChange = scrollChange / 36000.0;
-
-            Camera.ZoomOut((float) zoomChange);
-
-            Camera.cameraModel.PreviousScrollWheelValue = currentScrollWheelValue;
-
-            if (Math.Abs(zoomChange) < 0.000001) return;
         }
 
         /// <summary>
