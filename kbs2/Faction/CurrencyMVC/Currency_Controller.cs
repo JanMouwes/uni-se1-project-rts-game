7599e4e1e894b09e3d25a4b7dbfed5dc53823e1b
marijnkoel <marijnkoel@live.nl>
2018-11-23 14:26:08 +0100

    Currency controller and model

--- kbs2/Faction/CurrencyMVC/Currency_Controller.cs
+++ kbs2/Faction/CurrencyMVC/Currency_Controller.cs
@@ -1,10 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using kbs2.Faction.Enums;
+
 namespace kbs2.Faction.CurrencyMVC
 {
     public class Currency_Controller
     {
-        public Currency_Controller()
+        public Currency_Model model = new Currency_Model();
+
+        public void AddCurrency(float amount)
         {
+            if(amount>0){
+                model.currency += amount;
+            }
+        }
+
+        public void RemoveCurrency(float amount)
+        {
+            if (amount > 0)
+            {
+                model.currency -= amount;
+            }
+        }
+
+        public void OnTick() //TODO subscribe to onTick event
+        {
+            //todo this
+        }
+
+        public void AddResource(float amount, ResourceType resource)
+        {
+            if(amount>0){
+                model.resources[resource] += amount;
+            }
+        }
+
+        public void ResourcesToCurrency(){
+
+            float[] resourceTemp = new float[model.resources.Count];
+            int counter = 0;
+
+            foreach(KeyValuePair<ResourceType,float> x in model.resources ){
+
+                resourceTemp[counter] = x.Value;
+                counter++;
+            }
+            Array.Sort(resourceTemp);
+
+            float lastvalue = 0;
+
+            for (int i = 0; i < model.resources.Count;i++){
+                AddCurrency(resourceTemp[i] - lastvalue * (float)((model.resources.Count -1 - i) * 0.5));
+                lastvalue = resourceTemp[i];
+            }
         }
     }
 }
--- kbs2/Faction/CurrencyMVC/Currency_Model.cs
+++ kbs2/Faction/CurrencyMVC/Currency_Model.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using kbs2.Faction.Enums;
+
 namespace kbs2.Faction.CurrencyMVC
 {
     public class Currency_Model
     {
         public Currency_Model()
         {
+            foreach(ResourceType resource in Enum.GetValues(typeof(ResourceType ))){
+                resources.Add(resource, 0);
+            }
         }
+
+        public float currency; 
+        public Dictionary<ResourceType, float> resources;
     }
 }
