e19d249410f1f85e91544be633a92c873c395353
Jan Mouwes <jan.mouwes@gmail.com>
2018-11-23 14:39:37 +0100

    Merge branch 'Camera' of https://github.com/JanMouwes/SE1d_KBS2 into Camera
    # Conflicts:
    #	kbs2/View/Camera/CameraController.cs

--- kbs2/View/Camera/CameraController.cs
+++ kbs2/View/Camera/CameraController.cs
@@ -9,6 +9,9 @@ namespace kbs2.Desktop.View.Camera
         public CameraController(GraphicsDevice graphicsDevice) : base(graphicsDevice)
         {
             cameraModel = new CameraModel();
+
+            base.MinimumZoom = CameraModel.MinimumZoom;
+            base.MaximumZoom = CameraModel.MaximumZoom;
         }
 
         public CameraController(ViewportAdapter viewportAdapter) : this(viewportAdapter.GraphicsDevice)
