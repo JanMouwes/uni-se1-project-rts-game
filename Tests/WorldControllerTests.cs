295e77369d1d2ab3225dc8db147a85cd318b082c
Jan Mouwes <jan.mouwes@gmail.com>
2018-11-23 15:37:27 +0100

    Added UnitTests

--- Tests/WorldControllerTests.cs
+++ Tests/WorldControllerTests.cs
@@ -1,4 +1,5 @@
 using kbs2.World;
+using kbs2.World.Cell;
 using kbs2.World.Chunk;
 using NUnit.Framework;
 
@@ -7,19 +8,25 @@ namespace Tests
     [TestFixture]
     public class WorldControllerTests
     {
-        [TestCase()]
-        public void Test_Chunk_Generates_CorrectCellCoords(Coords chunkCoords,
-            Coords relativeCellCoords,
-            Coords expectedCellCoords)
+        [TestCase(1, 1, 3, 4, 23, 24)]
+        [TestCase(1, 2, 3, 4, 23, 44)]
+        public void Test_Chunk_Generates_CorrectCellCoords(int chunkX, int chunkY, int relativeCellX, int relativeCellY,
+            int expectedCellX, int expectedCellY)
         {
             //    Arrange
-            WorldChunkModel chunkModel = WorldChunkFactory.ChunkOfTerrainType()
-            
+            WorldChunkController chunk;
+            Coords chunkCoords = new Coords() {x = chunkX, y = chunkY};
+            Coords relativeCellCoords = new Coords() {x = relativeCellX, y = relativeCellY};
+            Coords expectedCellCoords = new Coords() {x = expectedCellX, y = expectedCellY};
+
             //    Act
-            
-            
+            chunk = WorldChunkFactory.ChunkOfTerrainType(chunkCoords, TerrainType.Soil);
+
+
             //    Assert
-            
+            WorldCellModel cell = chunk.worldChunkModel.grid[relativeCellCoords.x, relativeCellCoords.y];
+
+            Assert.AreEqual(cell.RealCoords, expectedCellCoords);
         }
     }
 }
