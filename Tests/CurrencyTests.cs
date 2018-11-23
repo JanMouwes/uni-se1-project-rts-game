c36f0f1985f4180223263bbc0c2bb6b2c9a8ae77
ridewo <ridewo@outlook.com>
2018-11-23 15:25:42 +0100

    resourcetocurrencytest

--- Tests/CurrencyTests.cs
+++ Tests/CurrencyTests.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using kbs2.Faction.CurrencyMVC;
+using kbs2.Faction.Enums;
 using NUnit.Framework;
 
 namespace Tests
 {
+    [TestFixture]
     public class CurrencyTests
     {
-        [TestFixture]
+        
         public CurrencyTests()
         {
+        }
+        Currency_Controller controller;
+
+       
+
+
+        [Test]
+        [TestCase(1,2,3,10+500)]
+        public void ResourcesToCurrencyTest(float type1, float type2, float type3, float result)
+        {
+            controller = new Currency_Controller();
+            controller.AddResource(type1, ResourceType.Food);
+            controller.AddResource(type1, ResourceType.Stone);
+            controller.AddResource(type1, ResourceType.Wood);
+
+            controller.ResourcesToCurrency();
+            Assert.AreEqual(controller.model.currency, result);
         }
     }
 }
--- kbs2/Faction/CurrencyMVC/Currency_Model.cs
+++ kbs2/Faction/CurrencyMVC/Currency_Model.cs
@@ -6,14 +6,17 @@ namespace kbs2.Faction.CurrencyMVC
 {
     public class Currency_Model
     {
+
+        public Dictionary<ResourceType, float> resources;
         public Currency_Model()
         {
-            foreach(ResourceType resource in Enum.GetValues(typeof(ResourceType ))){
+            resources = new Dictionary<ResourceType, float>();
+
+            foreach (ResourceType resource in Enum.GetValues(typeof(ResourceType ))){
                 resources.Add(resource, 0);
             }
         }
 
         public float currency = 500; 
-        public Dictionary<ResourceType, float> resources;
     }
 }
