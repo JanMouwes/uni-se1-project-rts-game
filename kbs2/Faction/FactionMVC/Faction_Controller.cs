540ca11c5af3db7c5b068180f75f34c453814fe6
Niels van Warmerdam <nielswarmerdam@hotmail.com>
2018-11-23 13:46:14 +0100

    Moved all the Faction_Model interfaces to the Faction_Controller Changed the IsHostileTo function to a lambda function

--- kbs2/Faction/FactionMVC/Faction_Controller.cs
+++ kbs2/Faction/FactionMVC/Faction_Controller.cs
@@ -1,4 +1,7 @@
-﻿using kbs2.Unit.Model;
+﻿using kbs2.Faction.Enums;
+using kbs2.Faction.Interfaces;
+using kbs2.Unit.Model;
+using kbs2.WorldEntity.Building;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +10,37 @@ using System.Threading.Tasks;
 
 namespace kbs2.Faction.FactionMVC
 {
-    public class Faction_Controller
+    public class Faction_Controller : IHasFactionBuilding, IHasFactionRelationship, IHasFactionUnit
     {
         static public Faction_Model FactionModel { get; set; }
 
+        public List<Building_Model> Buildings { get; set; }
+
+        public List<Unit_Model> Units { get; set; }
+        // Adds a unit to the faction units list
         Action<Unit_Model> AddUnitToFaction = unit => FactionModel.Units.Add(unit);
-        //[review] also change faction in unit
+        // Checks if the given faction is hostile to this faction
+        public bool IsHostileTo(Faction_Model faction) => FactionModel.FactionRelationships[faction] == Faction_Relations.hostile;
+        // Checks if there is a relation with the given faction and changes it to the given relation if not the same
+        public void ChangeRelationship(Faction_Model faction, Faction_Relations relation)
+        {
+            if(FactionModel.FactionRelationships[faction] != relation)
+            {
+                FactionModel.FactionRelationships.Remove(faction);
+                FactionModel.FactionRelationships.Add(faction, relation);
+
+                faction.FactionRelationships.Remove(FactionModel);
+                faction.FactionRelationships.Add(FactionModel, relation);
+            }
+        }
+        // Adds a relationship to the faction if it doesnt exist yet
+        public void AddRelationship(Faction_Model faction, Faction_Relations relation)
+        {
+            if (!FactionModel.FactionRelationships.ContainsKey(faction))
+            {
+                FactionModel.FactionRelationships.Add(faction, relation);
+                faction.FactionRelationships.Add(FactionModel, relation);
+            }
+        }
     }
 }
