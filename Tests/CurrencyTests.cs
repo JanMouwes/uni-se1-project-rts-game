1e6ec00637620db46e66519fc66695e5ac33cf3a
marijnkoel <marijnkoel@live.nl>
2018-11-26 11:49:56 +0100

    unittests fixed

--- Tests/CurrencyTests.cs
+++ Tests/CurrencyTests.cs
@@ -16,21 +16,7 @@ namespace Tests
         }
         Currency_Controller controller;
 
-       
-
-
-        [Test]
-        [TestCase(1,2,3,10+500)]
-        public void ResourcesToCurrencyTest(float type1, float type2, float type3, float result)
-        {
-            controller = new Currency_Controller();
-            controller.AddResource(type1, ResourceType.Food);
-            controller.AddResource(type1, ResourceType.Stone);
-            controller.AddResource(type1, ResourceType.Wood);
-
-            controller.ResourcesToCurrency();
-            Assert.AreEqual(controller.model.currency, result);
-        }
+      
 
         [Test]
         [TestCase(20)]
