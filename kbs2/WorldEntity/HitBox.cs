aa21aa62f7f89e0327febd78f00ec83f68beffe4
bluu1296 <binhluu96@gmail.com>
2018-11-28 11:07:39 +0100

    interfaces added en ingevuld

--- kbs2/WorldEntity/HitBox.cs
+++ kbs2/WorldEntity/HitBox.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using kbs2.World;
+using kbs2.WorldEntity.Interfaces;
 
 namespace kbs2.WorldEntity
 {
-	class HitBox
+	class HitBox : CircleHitBox, RectHitBox
 	{
+		public double Radius { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public double[,] Dimension { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+		public HitBox()
+		{
+
+		}
+
+		public bool CollideWith(HitBox hitbox)
+		{
+			return true;
+		}
+		public void CollideWith(Coords coords)
+		{
+
+		}
 	}
 }
---
+++ kbs2/WorldEntity/Interfaces/CircleHitBox.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kbs2.WorldEntity.Interfaces
+{
+	public interface CircleHitBox
+	{
+		double Radius { get; set; };
+	}
+}
