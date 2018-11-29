acc9eaf05f6f3346d8db80cd4b0333f2dc52d83b
bluu1296 <binhluu96@gmail.com>
2018-11-29 10:53:08 +0100

    wat dingetjes aangepast aan de checkclicked method

--- kbs2/GamePackage/Selection/Selection_Controller.cs
+++ kbs2/GamePackage/Selection/Selection_Controller.cs
@@ -20,11 +20,13 @@ namespace kbs2.GamePackage
         public Selection_View View { get; set; }
 
         public List<Unit_Controller> SelectedUnits { get; set; }
+		
 
-        public Selection_Controller(string lineTexture, MouseState mouseState)
+		public Selection_Controller(string lineTexture, MouseState mouseState)
         {
             Model = new Selection_Model(mouseState);
             View = new Selection_View(lineTexture);
+			SelectedUnits = new List<Unit_Controller>();
         }
 
         public void DrawSelectionBox(List<Unit_Controller> List, MouseState CurMouseState, Matrix viewMatrix, int tileSize)
@@ -50,27 +52,47 @@ namespace kbs2.GamePackage
 
         public void CheckClicked(List<Unit_Controller> List, MouseState CurMouseState, Matrix viewMatrix, int tileSize)
         {
-            if(CurMouseState.LeftButton == ButtonState.Pressed)
+			KeyboardState state = Keyboard.GetState();
+
+			if (CurMouseState.LeftButton == ButtonState.Pressed)
             {
                 Vector2 mousePosition = new Vector2(CurMouseState.X, CurMouseState.Y);
                 Vector2 worldPosition = Vector2.Transform(mousePosition, Matrix.Invert(viewMatrix));
+				
+				RectangleF MouselickPosition = new RectangleF((worldPosition.X / tileSize), (worldPosition.Y / tileSize), 0.01f, 0.01f);
 
-                foreach (Unit_Controller controller in List)
-                {
-                    RectangleF UnitClickBox = controller.CalcClickBox();
-                    
-                    RectangleF MouselickPosition = new RectangleF((worldPosition.X / tileSize), (worldPosition.Y / tileSize), 0.01f, 0.01f);
-                    if (MouselickPosition.Intersects(UnitClickBox))
-                    {
-                        controller.UnitView.ImageSrc = "pikachu_idle";
-                    }
-                }
-            }
+				if (SelectedUnits.Count() > 0)
+				{
+					SelectedUnits.Clear();
+
+				}
+				else
+				{
+					foreach (Unit_Controller unit in List)
+					{
+
+						RectangleF UnitClickBox = unit.CalcClickBox();
+						if (MouselickPosition.Intersects(UnitClickBox))
+						{
+							unit.UnitView.ImageSrc = "pikachu_idle";
+							SelectedUnits.Add(unit);
+						}
+						if (state.IsKeyDown(Keys.LeftControl) && MouselickPosition.Intersects(UnitClickBox))
+						{
+							unit.UnitView.ImageSrc = "pikachu_idle";
+							SelectedUnits.Add(unit);
+						}
+
+
+					}
+				}
+
+			}
         }
 
-        public void CheckClicked(List<Building_Controller> List, MouseState CurMouseState, Matrix viewMatrix, int tileSize)
-        {
+		public void CheckClicked(List<Building_Controller> List, MouseState CurMouseState, Matrix viewMatrix, int tileSize)
+		{
 
-        }
-    }
+		}
+	}
 }
