8955527e8785a7f646bad501fea8fdf33abc698e
chris <s1116195@student.windesheim.nl>
2018-11-29 12:06:14 +0100

    added an placeholder for a test

---
+++ Tests/WorldViewTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using kbs2.World.Chunk;
+
+namespace Tests
+{
+    [TestFixture]
+    class WorldViewTests
+    {
+        [TestCase()]
+        public Dictionary<Coords, WorldChunkController> Test_WorldView_Calculates_ChunksOnScreen()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
