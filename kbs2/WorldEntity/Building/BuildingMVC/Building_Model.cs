a3212604ef39b55f07e7f19fce848fd54575b219
ridewo <ridewo@outlook.com>
2018-11-29 11:08:26 +0100

    added building branch

--- kbs2/WorldEntity/Building/BuildingMVC/Building_Model.cs
+++ kbs2/WorldEntity/Building/BuildingMVC/Building_Model.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using kbs2.World;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace kbs2.WorldEntity.Building
 {
 	public class Building_Model
 	{
-	}
+        public Coords TopLeft { get; set; }
+    }
 }
