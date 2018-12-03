079263d371faae35e94899bf9f43e0c179f29ac3
chris <s1116195@student.windesheim.nl>
2018-12-03 12:26:03 +0100

    added worldView

---
+++ kbs2/World/World/WorldView.cs
@@ -0,0 +1,26 @@
+﻿using kbs2.Desktop.World.World;
+using kbs2.World.Cell;
+using kbs2.World.Chunk;
+using kbs2.World.Structs;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kbs2.World.World
+{
+    public class WorldView
+    {
+        public WorldModel worldModel { get; set; }
+
+        public WorldView(WorldModel world)
+        {
+            this.worldModel = world;
+        }
+
+    }
+}
