ab8a7e479462f219c0a4a6ff7d6a4f17d375a133
ridewo <ridewo@outlook.com>
2018-12-04 13:54:28 +0100

    made ISelectable

---
+++ kbs2/WorldEntity/Interfaces/ISelectable.cs
@@ -0,0 +1,14 @@
+﻿using MonoGame.Extended;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kbs2.WorldEntity.Interfaces
+{
+    public interface ISelectable
+    {
+        RectangleF CalcClickBox();
+    }
+}
