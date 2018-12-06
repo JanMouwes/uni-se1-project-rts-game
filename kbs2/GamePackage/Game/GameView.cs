f96b5440c9c161a1ec9de46f293b320b3455f151
Niels van Warmerdam <nielswarmerdam@hotmail.com>
2018-12-06 10:54:13 +0100

    gg

--- kbs2/GamePackage/Game/GameView.cs
+++ kbs2/GamePackage/Game/GameView.cs
@@ -81,7 +81,7 @@ namespace kbs2.GamePackage
 
 
             //TESTCODE
-            BuildingDef def = new BuildingDef();
+            /*BuildingDef def = new BuildingDef();
             def.BuildingShape = new List<Coords>
             {
                 new Coords { x = 0, y = 0 },
@@ -91,11 +91,11 @@ namespace kbs2.GamePackage
             };
             def.height = 2f;
             def.width = 2f;
-            def.imageSrc = "TrainingCenter";
+            def.imageSrc = "TrainingCenter";*/
 
-            //DBController.OpenConnection("DefDex");
-            //BuildingDef def = DBController.GetDefinitionBuilding(1);
-            //DBController.CloseConnection();
+            DBController.OpenConnection("DefDex");
+            BuildingDef def = DBController.GetDefinitionBuilding(1);
+            DBController.CloseConnection();
 
             Building_Controller building = BuildingFactory.CreateNewBuilding(def, new Coords { x = 0, y = 0 });
             gameModel.World.AddBuilding(def, building);
