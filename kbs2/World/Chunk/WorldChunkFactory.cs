b2435b711aa184ba7ad9ed46922dd13d9a8cc3ad
chris <s1116195@student.windesheim.nl>
2018-12-07 14:05:51 +0100

    added a more clean chunk generation

--- kbs2/World/Chunk/WorldChunkFactory.cs
+++ kbs2/World/Chunk/WorldChunkFactory.cs
@@ -29,5 +29,30 @@ namespace kbs2.World.Chunk
 
             return controller;
         }
+
+        public static WorldChunkController ChunkOfTerrain(Coords chunkCoords)
+        {
+            WorldChunkController controller = new WorldChunkController(chunkCoords);
+            controller.WorldChunkModel.grid = new WorldCellController[WorldChunkModel.ChunkSize, WorldChunkModel.ChunkSize];
+
+            for (int i = 0; i < Math.Pow(WorldChunkModel.ChunkSize, 2); i++)
+            {
+                //    Calculate chunk-relative X and Y (between 0 and ChunkSize)
+                int relXIndex = (int)i % WorldChunkModel.ChunkSize;
+                int relYIndex = (int)Math.Floor(i / (double)WorldChunkModel.ChunkSize);
+
+                controller.WorldChunkModel.grid[relXIndex, relYIndex] = WorldCellFactory.GetNewCell(new Structs.FloatCoords { x = chunkCoords.x * WorldChunkModel.ChunkSize + relXIndex, y = chunkCoords.y * WorldChunkModel.ChunkSize + relYIndex }, TerrainDef.TerrainDef.TerrainDictionairy[terrainType]);
+            }
+
+            foreach (WorldCellController worldCell in controller.WorldChunkModel.grid)
+            {
+                WorldCellModel worldCellModel = worldCell.worldCellModel;
+
+                worldCellModel.Terrain = worldCellModel.BaseTerrain = TerrainType.Grass;
+                worldCellModel.ParentChunk = controller.WorldChunkModel;
+            }
+
+            return controller;
+        }
     }
 }
