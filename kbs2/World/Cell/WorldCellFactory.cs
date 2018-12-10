7fa42a2d7cf1538337b3fd1044a04c8a38918a3c
chris <s1116195@student.windesheim.nl>
2018-12-10 19:14:06 +0100

    probs working idk

--- kbs2/World/Cell/WorldCellFactory.cs
+++ kbs2/World/Cell/WorldCellFactory.cs
@@ -1,4 +1,5 @@
-﻿using kbs2.World.Structs;
+﻿using kbs2.World.Enums;
+using kbs2.World.Structs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,14 @@ namespace kbs2.World.Cell
 {
     class WorldCellFactory
     {
-        public static WorldCellController GetNewCell(FloatCoords coords, string texture)
+        private static FastNoise myNoise = new FastNoise();
+
+        public static WorldCellController GetNewCell(FloatCoords coords)
         {
+            myNoise.SetNoiseType(FastNoise.NoiseType.Perlin); // Set the desired noise type
+
+            TerrainType textureType = (myNoise.GetNoise(coords.x, coords.y) > 0) ? TerrainType.Grass: TerrainType.Water;
+            string texture = TerrainDef.TerrainDef.TerrainDictionary[textureType];
             return new WorldCellController(coords, texture);
         }
     }
