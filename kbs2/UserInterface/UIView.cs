2597a81c1850239f601186a9f54208c164dffe0f
marijnkoel <marijnkoel@live.nl>
2018-12-10 13:36:35 +0100

    iets

---
+++ kbs2/UserInterface/UIView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using kbs2.GamePackage.Interfaces;
+using kbs2.World;
+using kbs2.World.Structs;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace kbs2.UserInterface
+{
+    public class UIView : IViewable
+    {
+
+        public Coords coords = new Coords
+        {
+            x = 0,
+            y = 0
+        };
+        
+        public FloatCoords Coords { get { return (FloatCoords)coords; } set {; } }
+        public float Height { get { return 120; } set {; } }
+        public float Width { get; set; }
+        public string Texture { get { return "UITexture"; } set {; } }
+        public Color Color { get { return Color.White; } set {; } }
+        public int ZIndex { get { return 1; } set {; } }
+    }
+}
