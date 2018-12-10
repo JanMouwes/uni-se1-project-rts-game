8572842fd9cc3be374d003302c152bb530f5c277
marijnkoel <marijnkoel@live.nl>
2018-12-10 16:38:57 +0100

    oops

--- kbs2/GamePackage/DayCycle/DayModel.cs
+++ kbs2/GamePackage/DayCycle/DayModel.cs
@@ -4,7 +4,7 @@ namespace kbs2.GamePackage.DayCycle
     public class DayModel
     {
         // day is 144 seconds 
-        public const int day = 14;
+        public const int day = 144;
 
         public int currentDay;
     }
