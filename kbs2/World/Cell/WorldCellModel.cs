3be3d8913ffea1389f1e253ce7eae2845b01415f
marijnkoel <marijnkoel@live.nl>
2018-12-13 16:39:33 +0100

    fix

--- kbs2/World/Cell/WorldCellModel.cs
+++ kbs2/World/Cell/WorldCellModel.cs
@@ -10,24 +10,24 @@ using kbs2.WorldEntity.Interfaces;
 
 namespace kbs2.World.Cell
 {
-	public class WorldCellModel
-	{
+    public class WorldCellModel
+    {
 
         // The base terrain defines the original terrain of this tile while the terrain keeps track of the current terrain state.
-		public TerrainType BaseTerrain { get; set; }
-		public TerrainType Terrain { get; set; }
+        public TerrainType BaseTerrain { get; set; }
+        public TerrainType Terrain { get; set; }
 
         // RealCoords are coords relative to the center tile
-		public Coords RealCoords { get; set; }
+        public Coords RealCoords { get; set; }
 
         // This defines what chunk the cell is in
-		public WorldChunkModel ParentChunk { get; set; }
+        public WorldChunkModel ParentChunk { get; set; }
 
         // ViewMode defines the current ViewMode Enum state ( full, fog or none )
         public ViewMode ViewMode { get; set; } = ViewMode.None;
 
         public IImpassable BuildingOnTop { get; set; }
-	}
+
         public WorldCellModel(TerrainType terrain, Coords realCoords)
         {
             this.Terrain = this.BaseTerrain = terrain;
