ccda8e241982da58f1937a408cb2bd667459d89f
ridewo <ridewo@outlook.com>
2018-12-22 14:13:22 +0100

    comments in fogcontroller

--- kbs2/GamePackage/FogController.cs
+++ kbs2/GamePackage/FogController.cs
@@ -21,35 +21,59 @@ namespace kbs2.GamePackage
         public Faction_Controller faction { get; set; }
         public WorldController worldController { get; set; }
 
+
+        /// <summary>
+        /// set everything in line of sight of units and buldings in the faction on the viewmode of your input
+        /// </summary>
+        /// <param name="mode"></param>
         public void UpdateViewModes(ViewMode mode)
         {
+            // line of sight units
             foreach(UnitController unit in faction.FactionModel.Units)
             {
                 UpdateViewMode(mode, unit.viewrange, unit.center);
             }
+            // lino of sight buildings
             foreach(IStructure building in faction.FactionModel.Buildings)
             {
                 UpdateViewMode(mode, building.viewrange, building.center);
             }
+            // set units in line of sight to full view
             UpdateUnits();
         }
 
+
+        /// <summary>
+        /// sets everything within viewrange from the coords to the specified viewmode
+        /// </summary>
+        /// <param name="mode"></param>
+        /// <param name="viewrange"></param>
+        /// <param name="coords"></param>
         public void UpdateViewMode(ViewMode mode , int viewrange, FloatCoords coords)
         {
+            // loop from -viewrange to + viewrange
             for (int x = (viewrange) * -1; x <= viewrange; x++)
             {
                 for (int y = (viewrange) * -1; y <= viewrange; y++)
                 {
+                    // set coords relative to the given coords
                     Coords tempcoords = (Coords)new FloatCoords { x = x + coords.x, y = y + coords.y };
+                    // check if the coords are within viewrange
                     if (!(DistanceCalculator.getDistance2d((FloatCoords)tempcoords, coords) < viewrange)) continue;
+                    // get the cell from the tempcoords
                     WorldCellController cellController = worldController.GetCellFromCoords(tempcoords);
+                    // check if the cellcontroller exists
                     if (cellController == null) continue;
+                    // set the viewmode
                     cellController.ChangeViewMode(mode);
+                    // check if there is a building in the cell
                     if (cellController.worldCellModel.BuildingOnTop == null) continue;
+                    // set viewmode of the building on the cell
                     if (cellController.worldCellModel.BuildingOnTop.GetType() == typeof(BuildingController))
                     {
                         ((BuildingController)cellController.worldCellModel.BuildingOnTop).View.ViewMode = mode;
                     }
+                    // set viewmode of the ConstructingBuilding on the cell
                     if (cellController.worldCellModel.BuildingOnTop.GetType() == typeof(ConstructingBuildingController))
                     {
                         ((ConstructingBuildingController)cellController.worldCellModel.BuildingOnTop).ConstructingBuildingView.ViewMode = mode;
@@ -58,6 +82,9 @@ namespace kbs2.GamePackage
             }
         }
 
+        /// <summary>
+        /// check for all units if they are on a visible cell and change viewmode acordingly
+        /// </summary>
         public void UpdateUnits()
         {
             foreach(UnitController unit in worldController.WorldModel.Units)
