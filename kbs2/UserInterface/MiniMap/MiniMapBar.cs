eac0b88835c10b441e95e37aecccd6dd64db14ad
Christiaan Kornaat <s1116195@student.windesheim.nl>
2019-01-02 17:46:16 +0100

    more cleaning up like: gui;

--- kbs2/UserInterface/MiniMap/MiniMapBar.cs
+++ kbs2/UserInterface/MiniMap/MiniMapBar.cs
@@ -8,30 +8,31 @@ using kbs2.GamePackage.Interfaces;
 using kbs2.World;
 using kbs2.World.Structs;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace kbs2.UserInterface
 {
 	public class MiniMapBar : IViewImage
 	{
-		public GameController gameController { get; set; }
+		public GraphicsDevice GraphicsDevice { get; set; }
 		public Coords coords => new Coords
 		{
 			x = 0,
-			y = (int)(gameController.GraphicsDevice.Viewport.Height * .70)
+			y = (int)(GraphicsDevice.Viewport.Height * .70)
 		};
 
 		public FloatCoords Coords { get { return (FloatCoords)coords; } set {; } }
-		public float Height { get { return (int)(gameController.GraphicsDevice.Viewport.Height * .30); } set {; } }
-		public float Width { get { return (int)(gameController.GraphicsDevice.Viewport.Width * .15); } set {; } }
+		public float Height { get { return (int)(GraphicsDevice.Viewport.Height * .30); } set {; } }
+		public float Width { get { return (int)(GraphicsDevice.Viewport.Width * .15); } set {; } }
 		public string Texture { get { return "bottombarleft"; } set {; } }
 		public Color Colour { get { return Color.White; } set {; } }
 		public int ZIndex { get { return 1; } set {; } }
 
         public ViewMode ViewMode => ViewMode.Full;
 
-        public MiniMapBar(GameController gameController)
+        public MiniMapBar(GraphicsDevice GraphicsDevice)
 		{
-			this.gameController = gameController;
+			this.GraphicsDevice = GraphicsDevice;
 		}
 	}
 }
